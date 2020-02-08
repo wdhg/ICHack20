96c146ed17c4dc4f1a6e35c3266f88861167cecc
wdhg <wdhgrant@gmail.com>
2020-02-08 20:24:19 +0000

    remove Camera from World2D

--- engine/src/World2D.java
+++ engine/src/World2D.java
@@ -3,20 +3,14 @@ import java.util.List;
 
 public class World2D {
 
-  Camera camera;
   List<GameObject2D> gameObjects;
 
   public World2D() {
-    this.camera = new Camera(new Vector3(0, 0, 0));
     this.gameObjects = new ArrayList<>();
   }
 
   public void addGameObject(GameObject2D gameObject) {
     this.gameObjects.add(gameObject);
-  }
-
-  public Camera getCamera() {
-    return this.camera;
   }
 
   public List<GameObject2D> getGameObjects() {
