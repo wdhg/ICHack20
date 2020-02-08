1555469ff5d7f8a182ef05a46882cd0be9ec67a9
jml19 <jessica.lally19@imperial.ac.uk>
2020-02-08 23:57:29 +0000

    add everything cause im bad

--- engine/tetris/src/TetrisShape.java
+++ engine/tetris/src/TetrisShape.java
@@ -1,119 +1,60 @@
 import java.util.Random;
 
-public class Shape extends GameObject2D {
+public class TetrisShape {
 
-  private Tetrominoe pieceShape;
-  private int[][] coords;
+  private Tetrominoe shape;
+  public int[] xs;
+  public int[] ys;
 
-  public Shape() {
-
-    coords = new int[4][2];
-    setShape(Tetrominoe.NoShape);
+  public TetrisShape() {
+    this.shape = setRandomShape();
+    initialiseShape();
   }
 
-  void setShape(Tetrominoe shape) {
-
-    int[][][] coordsTable = new int[][][]{
-        {{0, 0}, {0, 0}, {0, 0}, {0, 0}},
-        {{0, -1}, {0, 0}, {-1, 0}, {-1, 1}},
-        {{0, -1}, {0, 0}, {1, 0}, {1, 1}},
-        {{0, -1}, {0, 0}, {0, 1}, {0, 2}},
-        {{-1, 0}, {0, 0}, {1, 0}, {0, 1}},
-        {{0, 0}, {1, 0}, {0, 1}, {1, 1}},
-        {{-1, -1}, {0, -1}, {0, 0}, {0, 1}},
-        {{1, -1}, {0, -1}, {0, 0}, {0, 1}}
-    };
-
-    for (int i = 0; i < 4; i++) {
-
-      System.arraycopy(coordsTable[shape.ordinal()], 0, coords, 0, 4);
+  private void initialiseShape() {
+    switch (shape) {
+      case ZShape:
+        xs = new int[]{4, 4, 5, 5};
+        ys = new int[]{0, 1, 1, 2};
+        break;
+      case SShape:
+        xs = new int[]{5, 5, 4, 4};
+        ys = new int[]{0, 1, 1, 2};
+        break;
+      case LineShape:
+        xs = new int[]{5, 5, 5, 5};
+        ys = new int[]{0, 1, 2, 3};
+        break;
+      case TShape:
+        xs = new int[]{4, 4, 4, 5};
+        ys = new int[]{0, 1, 1, 2};
+        break;
+      case SquareShape:
+        xs = new int[]{4, 5, 4, 5};
+        ys = new int[]{0, 0, 1, 1};
+        break;
+      case LShape:
+        xs = new int[]{4, 4, 4, 5};
+        ys = new int[]{0, 1, 2, 2};
+        break;
+      case MirroredLShape:
+        xs = new int[]{4, 5, 5, 5};
+        ys = new int[]{0, 1, 2, 2};
+        break;
     }
-
-    pieceShape = shape;
   }
 
-  private void setX(int index, int x) {
-
-    coords[index][0] = x;
+  public Tetrominoe getShape() {
+    return shape;
   }
 
-  private void setY(int index, int y) {
-
-    coords[index][1] = y;
-  }
-
-  int x(int index) {
-
-    return coords[index][0];
-  }
-
-  int y(int index) {
-
-    return coords[index][1];
-  }
-
-  Tetrominoe getShape() {
-
-    return pieceShape;
-  }
-
-  void setRandomShape() {
+  private Tetrominoe setRandomShape() {
 
     var r = new Random();
     int x = Math.abs(r.nextInt()) % 7 + 1;
 
     Tetrominoe[] values = Tetrominoe.values();
-    setShape(values[x]);
+    return values[x];
   }
 
-  public int minX() {
-
-    int m = coords[0][0];
-
-    for (int i = 0; i < 4; i++) {
-
-      m = Math.min(m, coords[i][0]);
-    }
-
-    return m;
-  }
-
-
-  int minY() {
-
-    int m = coords[0][1];
-
-    for (int i = 0; i < 4; i++) {
-
-      m = Math.min(m, coords[i][1]);
-    }
-
-    return m;
-  }
-
-  private Shape rotateLeft() {
-    if (pieceShape == Tetrominoe.SquareShape) {
-      return this;
-    }
-    super.rotate(-90);
-  }
-
-  Shape rotateRight() {
-
-    if (pieceShape == Tetrominoe.SquareShape) {
-
-      return this;
-    }
-
-    var result = new Shape();
-    result.pieceShape = pieceShape;
-
-    for (int i = 0; i < 4; i++) {
-
-      result.setX(i, -y(i));
-      result.setY(i, x(i));
-    }
-
-    return result;
-  }
 }
