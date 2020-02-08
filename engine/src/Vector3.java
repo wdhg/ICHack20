645a4d34801c81c55f55b6ef03279d748c944bf5
wdhg <wdhgrant@gmail.com>
2020-02-08 13:55:39 +0000

    implement basic Vector3 and camera

--- engine/src/Vector3.java
+++ engine/src/Vector3.java
@@ -1,2 +1,27 @@
 public class Vector3 {
+  private double x, y, z;
+
+  public Vector3() {
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
+  }
+
+  public Vector3(double x, double y, double z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  public double getX() {
+    return x;
+  }
+
+  public double getY() {
+    return y;
+  }
+
+  public double getZ() {
+    return z;
+  }
 }
