948f178db9941aca552dd6de69977971d6bf859a
Noor Sawhney <noor@dyn898-235.eduroam.ic.ac.uk>
2020-02-08 16:38:16 +0000

    added pong

--- engine/src/Shape.java
+++ engine/src/Shape.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
 public class Shape {
+
+    public ArrayList<Vector2> normalise(ArrayList<Vector2> points, double minBound, double maxBound) {
+        ArrayList<Vector2> normalisedPoints = new ArrayList<>();
+        for (Vector2 point : points) {
+            double x = point.getX();
+            double y = point.getY();
+            double newX = (maxBound - minBound) *
+                    (x - getMinX(points.stream()) / (getMaxX(points.stream()) - getMinX(points.stream()))) + minBound;
+            double newY = (maxBound - minBound) *
+                    (y - getMinY(points.stream()) / (getMaxY(points.stream()) - getMinY(points.stream()))) + minBound;
+
+            normalisedPoints.add(new Vector2(newX, newY));
+        }
+        return normalisedPoints;
+    }
+
+    private double getMaxX(Stream<Vector2> points) {
+        return points
+                .map(Vector2::getX)
+                .reduce(0.0, Math::max);
+    }
+
+    private double getMinX(Stream<Vector2> points) {
+        return points
+                .map(Vector2::getX)
+                .reduce(0.0, Math::min);
+    }
+
+    private double getMaxY(Stream<Vector2> points) {
+        return points
+                .map(Vector2::getY)
+                .reduce(0.0, Math::max);
+    }
+
+    private double getMinY(Stream<Vector2> points) {
+        return points
+                .map(Vector2::getY)
+                .reduce(0.0, Math::min);
+    }
 }
