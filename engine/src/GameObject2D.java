239dd83a7aab0f3cffbe067f20a359de42f5f354
wdhg <wdhgrant@gmail.com>
2020-02-08 18:39:42 +0000

    add basic GameObject2D class

--- engine/src/GameObject2D.java
+++ engine/src/GameObject2D.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class GameObject2D {
+  private Vector2 position;
+  private double rotation;
+  private Shape shape;
+
+  public GameObject2D(Vector2 position, double rotation, Shape shape) {
+    this.position = position;
+    this.rotation = rotation;
+    this.shape = shape;
+  }
+
+  public void move(Vector2 delta) {
+
+  }
+
+  public void rotate(double delta) {
+
+  }
+
+  public boolean collidesWith(GameObject2D other) {
+    return false;
+  }
 }
