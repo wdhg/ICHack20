b2c50f927beeb4a6ce9349d7666d95edbb8e075b
wdhg <wdhgrant@gmail.com>
2020-02-08 18:44:16 +0000

    add setters to Shape

--- engine/src/Shape.java
+++ engine/src/Shape.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public abstract class Shape {
@@ -5,8 +6,21 @@ public abstract class Shape {
   private List<Vector2> vertices;
   private List<Integer> edgeData;
 
+  public Shape() {
+    this.vertices = new ArrayList<>();
+    this.edgeData = new ArrayList<>();
+  }
+
   public Shape(List<Vector2> vertices, List<Integer> edgeData) {
     this.vertices = vertices;
+    this.edgeData = edgeData;
+  }
+
+  public void setVertices(List<Vector2> vertices) {
+    this.vertices = vertices;
+  }
+
+  public void setEdgeData(List<Integer> edgeData) {
     this.edgeData = edgeData;
   }
 }
