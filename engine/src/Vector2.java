37a2cec3d51680fdd962ea904e6ff9cd706ec09d
wdhg <wdhgrant@gmail.com>
2020-02-08 18:49:05 +0000

    reindent and implement add

--- engine/src/Vector2.java
+++ engine/src/Vector2.java
@@ -1,26 +1,31 @@
 public class Vector2 {
 
-    private double x;
-    private double y;
+  private double x;
+  private double y;
 
-    public Vector2(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
+  public Vector2(double x, double y) {
+    this.x = x;
+    this.y = y;
+  }
 
-    public double getX() {
-        return x;
-    }
+  public double getX() {
+    return x;
+  }
 
-    public double getY() {
-        return y;
-    }
+  public double getY() {
+    return y;
+  }
 
-    public void setX(double x) {
-        this.x = x;
-    }
+  public void setX(double x) {
+    this.x = x;
+  }
 
-    public void setY(double y) {
-        this.y = y;
-    }
+  public void setY(double y) {
+    this.y = y;
+  }
+
+  public void add(Vector2 other) {
+    this.setX(this.getX() + other.getX());
+    this.setY(this.getY() + other.getY());
+  }
 }
