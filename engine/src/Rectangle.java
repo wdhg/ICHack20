f70a28c8e3ac80af3bcc7554d62ea8995c511fd8
wdhg <wdhgrant@gmail.com>
2020-02-08 19:04:33 +0000

    add comments

--- engine/src/Rectangle.java
+++ engine/src/Rectangle.java
@@ -5,10 +5,10 @@ public class Rectangle extends Shape {
   public Rectangle(Vector2 position, double width, double height) {
     this.setVertices(
       Arrays.asList(
-        position,
-        position.add(width, 0),
-        position.add(width, height),
-        position.add(0, height)
+        position,                    // bottom left
+        position.add(width, 0),      // bottom right
+        position.add(width, height), // top right
+        position.add(0, height)      // top left
       )
     );
     this.setEdgeData(Arrays.asList(0,1,1,2,2,3,3,4));
