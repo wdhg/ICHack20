8bad306081ff45537cfaa37f6bcfe00e55fa80b7
wdhg <wdhgrant@gmail.com>
2020-02-08 15:10:04 +0000

    fix typo in vertices

--- engine/src/Mesh.java
+++ engine/src/Mesh.java
@@ -2,16 +2,16 @@ import java.util.ArrayList;
 
 public class Mesh {
 
-  private ArrayList<Vector3> verticies;
+  private ArrayList<Vector3> vertices;
   private ArrayList<Integer> edgeData;
 
   public Mesh() {
-    this.verticies = new ArrayList<>();
+    this.vertices = new ArrayList<>();
     this.edgeData = new ArrayList<>();
   }
 
-  public Mesh(ArrayList<Vector3> verticies, ArrayList<Integer> edgeData) {
-    this.verticies = verticies;
+  public Mesh(ArrayList<Vector3> vertices, ArrayList<Integer> edgeData) {
+    this.vertices = vertices;
     this.edgeData = edgeData;
   }
 }
