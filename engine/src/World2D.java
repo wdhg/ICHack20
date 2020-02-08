a47b5537bf0431313c255c7f5442d07cc3a151f5
wdhg <wdhgrant@gmail.com>
2020-02-08 18:56:04 +0000

    implement addGameObject add getters

--- engine/src/World2D.java
+++ engine/src/World2D.java
@@ -10,4 +10,16 @@ public class World2D {
     this.camera = new Camera(new Vector3(0, 0, 0));
     this.gameObjects = new ArrayList<>();
   }
+
+  public void addGameObject(GameObject2D gameObject) {
+    this.gameObjects.add(gameObject);
+  }
+
+  public Camera getCamera() {
+    return this.camera;
+  }
+
+  public List<GameObject2D> getGameObjects() {
+    return this.gameObjects;
+  }
 }
