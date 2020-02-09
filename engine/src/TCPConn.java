93af36335447266816f51f4cfd272413dad1e80a
Andy Wang <cbeuw.andy@gmail.com>
2020-02-09 05:18:37 +0000

    Fix tcp

--- engine/src/TCPConn.java
+++ engine/src/TCPConn.java
@@ -13,10 +13,11 @@ public class TCPConn {
     public void send(String json) throws IOException {
         byte[] jsonB = json.getBytes();
         int len = jsonB.length;
-        byte[] lenB = new byte[]{
-                (byte) (len >>> 8),
-                (byte) (len)
-        };
+        byte[] lenB = new byte[2];
+        lenB[0] = ((byte) (len >> 8));
+        lenB[0] &= 0xFF;
+        lenB[1] = ((byte) (len));
+        lenB[1] &= 0xFF;
         byte[] c = new byte[2 + jsonB.length];
         System.arraycopy(lenB, 0, c, 0, 2);
         System.arraycopy(jsonB, 0, c, 2, jsonB.length);
