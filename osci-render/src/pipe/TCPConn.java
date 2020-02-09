f6f1687bca560adc909cf413ed3317ac22e1d6ce
Andy Wang <cbeuw.andy@gmail.com>
2020-02-09 04:05:54 +0000

    Use tcp conn

---
+++ osci-render/src/pipe/TCPConn.java
@@ -0,0 +1,39 @@
+package pipe;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Arrays;
+
+public class TCPConn {
+    private int bufLen = 10240;
+    private Socket socket;
+    private byte[] buf = new byte[bufLen];
+    private InputStream input;
+
+    TCPConn(int port) throws IOException {
+        this.socket = new Socket("localhost", port);
+        input = socket.getInputStream();
+    }
+
+    // blocks
+    String getData() throws IOException {
+        byte[] lenB = new byte[2];
+        input.read(lenB);
+        int len = lenB[0] << 8 + lenB[1];
+
+        int pointer = 0;
+        for (;pointer < len;){
+            int red = input.read(buf, pointer, len-pointer);
+            pointer += red;
+        }
+
+
+        return new String(Arrays.copyOf(buf, len));
+    }
+
+}
